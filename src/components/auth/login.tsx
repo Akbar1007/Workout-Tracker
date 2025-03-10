17d2e1861d48f4e3d02ad64681df6deee02475a1
Akbar1007 <a.akbarjon007@gmail.com>
2025-03-10 06:47:12 +0500

    sign up/in

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,5 +1,39 @@
+import { useAuthState } from '@/stores/auth.store'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Separator } from '../ui/separator'
+
 const Login = () => {
-	return <div>Login</div>
+	const { setAuth } = useAuthState()
+
+	return (
+		<div className='flex flex-col'>
+			<h1 className='font-bold text-xl'>Login</h1>
+			<p className='text-muted-foreground'>
+				Do not have an account?{' '}
+				<span
+					className='text-blue-500 cursor-pointer hover:underline'
+					onClick={() => setAuth('register')}
+				>
+					Sign up
+				</span>
+			</p>
+
+			<Separator className='my-3' />
+
+			<div>
+				<span>Email</span>
+				<Input placeholder='YourName@gmail.com' />
+			</div>
+
+			<div className='mt-2'>
+				<span>Password</span>
+				<Input placeholder='********' type='password' />
+			</div>
+
+			<Button className='w-full h-12 mt-2'>Login</Button>
+		</div>
+	)
 }
 
 export default Login
--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -1,5 +1,46 @@
+import { useAuthState } from '@/stores/auth.store'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Separator } from '../ui/separator'
+
 const Register = () => {
-	return <div>Register</div>
+	const { setAuth } = useAuthState()
+
+	return (
+		<div className='flex flex-col'>
+			<h1 className='font-bold text-xl'>Register</h1>
+			<p className='text-muted-foreground'>
+				Already have an account?{' '}
+				<span
+					className='text-blue-500 cursor-pointer hover:underline'
+					onClick={() => setAuth('login')}
+				>
+					Sign in
+				</span>
+			</p>
+
+			<Separator className='my-3' />
+
+			<div>
+				<span>Email</span>
+				<Input placeholder='YourName@gmail.com' />
+			</div>
+
+			<div className='grid grid-cols-2 gap-4 mt-2'>
+				<div className='mt-2'>
+					<span>Password</span>
+					<Input placeholder='********' type='password' />
+				</div>
+
+				<div className='mt-2'>
+					<span>Confirm Password</span>
+					<Input placeholder='********' type='password' />
+				</div>
+			</div>
+
+			<Button className='w-full h-12 mt-2'>Register</Button>
+		</div>
+	)
 }
 
 export default Register
