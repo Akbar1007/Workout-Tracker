022c23635e0d6c314dc2a9ac022301ecdfc271e2
Akbar1007 <a.akbarjon007@gmail.com>
2025-03-11 15:14:38 +0500

    register validation

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -2,5 +2,16 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
 	email: z.string().email('Invalid email'),
-	password: z.string().min(8, 'Wrong'),
+	password: z.string().min(8, 'Wrong password'),
 })
+
+export const registerSchema = z
+	.object({
+		email: z.string().email('Invalid email'),
+		password: z.string().min(8, 'Wrong password'),
+		confirmPassword: z.string(),
+	})
+	.refine(data => data.password === data.confirmPassword, {
+		message: 'Password do not match.',
+		path: ['confirmPassword'],
+	})
